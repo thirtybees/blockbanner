77dd84664fd8f1513c71fb05c795767c3d1a7240
Michael Dekker <git@michaeldekker.nl>
2017-10-26 01:22:59 +0200

    Set version to 2.0.1

--- blockbanner.php
+++ blockbanner.php
@@ -41,7 +41,7 @@ class BlockBanner extends Module
     {
         $this->name = 'blockbanner';
         $this->tab = 'front_office_features';
-        $this->version = '2.0.2';
+        $this->version = '2.0.1';
         $this->author = 'thirty bees';
         $this->need_instance = 0;
 
