221dc139ae77a750504034ca774ea71cc8a1983b
Markus Hitter <mah@jump-ing.de>
2018-06-09 15:08:17 +0200

    Add missing, mandatory main class constructor entries.

--- blockbanner.php
+++ blockbanner.php
@@ -54,6 +54,7 @@ class BlockBanner extends Module
 
         $this->displayName = $this->l('Block Banner');
         $this->description = $this->l('Displays a banner at the top of the shop.');
+        $this->tb_versions_compliancy = '> 1.0.0';
         $this->ps_versions_compliancy = ['min' => '1.6', 'max' => '1.6.99.99'];
     }
 
